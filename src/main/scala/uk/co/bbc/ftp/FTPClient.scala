7dde880a026388b7de3eb2877d10ed97999c3dfc
Adamos Loizou <adamos.loizou@ovoenergy.com>
2015-04-29 13:54:20 +0100

    Removed warning for FTPClient hiding.

--- src/main/scala/uk/co/bbc/ftp/FTPClient.scala
+++ src/main/scala/uk/co/bbc/ftp/FTPClient.scala
@@ -1,8 +1,8 @@
 package uk.co.bbc.ftp
 
-import org.apache.commons.net.ftp.FTPClient
+import org.apache.commons.net.ftp.{FTPClient => ApacheFTPClient}
 
 object FTPClient {
   def apply (): FTP =
-    new FTP(new FTPClient)
+    new FTP(new ApacheFTPClient)
 }
